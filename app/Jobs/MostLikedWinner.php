a4aa8d0f3df19b79b6f7c0a935a9d6d342c10975
Yosef Salmalian <flaslnm@gmail.com>
2021-09-26 11:53:47 +0330

    cron job for 24:00

---
+++ app/Jobs/MostLikedWinner.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\Http;
+use App\Models\Post;
+
+class MostLikedWinner implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+        $post = Post::withCount('likes')
+                ->orderBy('likes_count', 'desc')
+                ->first();
+        Http::post('https://gorest.co.in/public/v1/users', 
+            $post->toArray());
+    }
+}
